075e128772eec00846e22718fbe236f6289c5ce9
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-17 20:51:44 +0900

    Describe declarations of gloval variables

---
+++ extern.h
@@ -0,0 +1,14 @@
+#ifndef _EXTERN_H_
+#define _EXTERN_H_
+
+// Standard Library
+#include <list>
+#include <string>
+
+namespace li
+{
+	extern std::list<std::string>	lines;
+	extern std::string		filename;
+}
+
+#endif
