dcb468e292534fc09058f466e01530d7d07ef87d
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-19 21:12:27 +0900

    Modify li::CommandProcess function

--- CommandProcess.cpp
+++ CommandProcess.cpp
@@ -12,16 +12,16 @@ using namespace std;
 
 void li::CommandProcess(const string &command)
 {
-	if(command == "f")
+	if(command == "f") // Print current filename
 		cout << filename << endl;
-	else if(command == "w")
+	else if(command == "w") // Write to current file
 	{
 		if(filename == "")
 			cerr << "Error: Please tell me output filename" << endl;
 		else
 			command::w(filename);
 	}
-	else if(regex_match(command, regex("w .+")))
+	else if(regex_match(command, regex("w .+"))) // Write to a file
 	{
 		char format[FORMAT_MAX], _filename[FILENAME_MAX];
 		sprintf(format, "w %%%us", (sizeof(_filename) / sizeof(char)));
@@ -29,49 +29,49 @@ void li::CommandProcess(const string &command)
 
 		command::w(_filename);
 	}
-	else if(regex_match(command, regex("e \\d+")))
+	else if(regex_match(command, regex("e \\d+"))) // Edit a line
 	{
 		int lineno;
 		sscanf(command, "e %d", &lineno);
 
 		command::e(--lineno);
 	}
-	else if(regex_match(command, regex("r \\d+")))
+	else if(regex_match(command, regex("r \\d+"))) // Remove a line
 	{
 		int lineno;
 		sscanf(command, "r %d", &lineno);
 
 		command::r(--lineno);
 	}
-	else if(regex_match(command, regex("i \\d+")))
+	else if(regex_match(command, regex("i \\d+"))) // Insert a line
 	{
 		int lineno;
 		sscanf(command, "i %d", &lineno);
 
 		command::i(--lineno);
 	}
-	else if(regex_match(command, regex("es \\d+")))
+	else if(regex_match(command, regex("es \\d+"))) // Edit lines
 	{
 		int lineno;
 		sscanf(command, "es %d", &lineno);
 
 		command::es(--lineno);
 	}
-	else if(regex_match(command, regex("is \\d+")))
+	else if(regex_match(command, regex("is \\d+"))) // Insert lines
 	{
 		int lineno;
 		sscanf(command, "is %d", &lineno);
 
 		command::is(--lineno);
 	}
-	else if(command == "o")
+	else if(command == "o") // Open current file again
 	{
 		if(filename == "")
 			cerr << "Error: Please tell me open filename" << endl;
 		else
 			command::o(filename);
 	}
-	else if(regex_match(command, regex("o .+")))
+	else if(regex_match(command, regex("o .+"))) // Open a file
 	{
 		char format[FORMAT_MAX], _filename[FILENAME_MAX];
 		sprintf(format, "o %%%us", (sizeof(_filename) / sizeof(char)));
@@ -79,9 +79,9 @@ void li::CommandProcess(const string &command)
 
 		command::o(_filename);
 	}
-	else if(command == "p")
+	else if(command == "p") // Print all lines
 		command::p(0, lines.size() - 1);
-	else if(regex_match(command, regex("p \\d+")))
+	else if(regex_match(command, regex("p \\d+"))) // Print a line
 	{
 		int lineno;
 		sscanf(command, "p %d", &lineno);
@@ -89,14 +89,14 @@ void li::CommandProcess(const string &command)
 
 		command::p(lineno, lineno);
 	}
-	else if(regex_match(command, regex("p \\d+ \\d+")))
+	else if(regex_match(command, regex("p \\d+ \\d+"))) // Print lines
 	{
 		int from, to;
 		sscanf(command, "p %d %d", &from, &to);
 
 		command::p(--from, --to);
 	}
-	else if(command == "q")
+	else if(command == "q") // quit
 		exit(EXIT_SUCCESS);
 	else
 		throw li::exception(0x2, "unknown command");
