432b1dee1624de219c937a749645b9aff7f06946
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-20 13:47:18 +0900

    Bugfix main function

--- main.cpp
+++ main.cpp
@@ -35,6 +35,7 @@ int main(int argc, char **argv)
 			{
 				cout << ">";
 				int YorN = getchar();
+				scanf("%*c");
 				if(YorN == EOF)
 					continue;
 
@@ -56,6 +57,7 @@ int main(int argc, char **argv)
 
 				break;
 			}
+			cout << endl;
 		}
 		else if(argc == 2)
 			filename = argv[1];
